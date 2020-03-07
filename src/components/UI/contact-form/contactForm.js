ea3179c4259ab186e37cfc7af8be9d7a286a3745
Artur Molenda <ar2rweb@gmail.com>
2020-03-07 16:09:45 +0100

    Final commit

--- src/components/UI/contact-form/contactForm.js
+++ src/components/UI/contact-form/contactForm.js
@@ -14,6 +14,8 @@ const ContactForm = () => {
         data-netlify="true"
         data-netlify-honeypot="bot-field"
       >
+        <input type="hidden" name="bot-field" />
+        <input type="hidden" name="form-name" value="contact" />
         <label>
           <input
             type="text"
